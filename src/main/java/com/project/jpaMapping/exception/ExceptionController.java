3d37b5f50c374533f305916695bb1071daf8f112
ayush <ayushofficial.in@gmail.com>
2024-11-16 03:10:48 +0530

    globalException_ResponseEntityExceptionHandler

--- src/main/java/com/project/jpaMapping/exception/ExceptionController.java
+++ src/main/java/com/project/jpaMapping/exception/ExceptionController.java
@@ -1,9 +1,13 @@
 package com.project.jpaMapping.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import jakarta.persistence.EntityNotFoundException;
@@ -22,6 +26,13 @@ public class ExceptionController extends ResponseEntityExceptionHandler {
 	public ResponseEntity<?> handleAllException(Exception ex) {
 		ex.printStackTrace();
 		return new ResponseEntity<String>("exception ", HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex,
+			HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		// TODO Auto-generated method stub
+		return new ResponseEntity<>("wrong http request", HttpStatus.NOT_FOUND);
 	}
 
 }
